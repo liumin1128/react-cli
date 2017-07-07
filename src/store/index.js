40b92305f4a1f4c66f5e4f9ca845f49f3b6f660f
liumin1128 <970568830@qq.com>
2017-07-07 08:34:34 +0800

    修复REDUX_DEVTOOLS在Safari报错的bug

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ const store = createStore(
     applyMiddleware(
       saga,
     ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : applyMiddleware(),
   ),
 );
 
